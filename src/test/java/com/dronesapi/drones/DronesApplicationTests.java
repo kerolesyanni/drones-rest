6a3ca54d0d74cedaeb36b53c29a4c410fe0d2d7c
kyanni <keroles.yanni@momkn.com.eg>
2022-11-19 22:38:42 +0200

    Add project to git that downloaded from spring.io

---
+++ src/test/java/com/dronesapi/drones/DronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dronesapi.drones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
